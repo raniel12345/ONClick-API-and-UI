10a4c6aca1d6e9adb586a8bfe9d4e0a2aff18838
Raniel Garcia <ranielgarcia2596@gmail.com>
2019-12-28 21:32:46 +0800

    Add groups loader

---
+++ server/src/loaders/group.js
@@ -0,0 +1,13 @@
+import Sequelize from "sequelize";
+
+export const barchGroups = async (keys, store) => {
+  const groups = await store.Group.findAll({
+    where: {
+      id: {
+        [Sequelize.Op.in]: keys
+      }
+    }
+  });
+
+  return keys.map(key => groups.find(group => group.id === key));
+};
