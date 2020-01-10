f6d38c67d0e377f1901a1c536e6961698476eb0c
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-01-11 00:14:00 +0800

    Do not validate token when is not provided

--- server/src/index.js
+++ server/src/index.js
@@ -1,112 +1,110 @@
-import { ApolloServer } from "apollo-server";
-import { AuthenticationError } from "apollo-server";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
+import { ApolloServer } from 'apollo-server';
+import { AuthenticationError } from 'apollo-server';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
-import DataLoader from "dataloader";
-import dataLoaders from "./loaders";
+import DataLoader from 'dataloader';
+import dataLoaders from './loaders';
 
-import typeDefs from "./schema";
-import resolvers from "./resolvers";
-import models, { sequelize } from "./models";
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models, { sequelize } from './models';
 
-import UserAPI from "./datasource/user";
-import ProjectAPI from "./datasource/project";
-import MemberAPI from "./datasource/member";
-import ProjectStatusAPI from "./datasource/projectStatus";
-import GroupAPI from "./datasource/group";
-import FeatureAPI from "./datasource/feature";
-import IssueAPI from "./datasource/issue";
+import UserAPI from './datasource/user';
+import ProjectAPI from './datasource/project';
+import MemberAPI from './datasource/member';
+import ProjectStatusAPI from './datasource/projectStatus';
+import GroupAPI from './datasource/group';
+import FeatureAPI from './datasource/feature';
+import IssueAPI from './datasource/issue';
 
-const is_production = process.env.NODE_ENV === "production" ? false : true;
+const is_production = process.env.NODE_ENV === 'production' ? false : true;
 
 // the function that sets up the global context for each resolver, using the req
 const context = async ({ req }) => {
-  /**
-   * Authentitication
-   */
-  const token = req.headers["authorization"];
-  let user = null;
-  if (token) {
-    try {
-      user = await jwt.verify(token, process.env.SECRET_KEY);
-    } catch (e) {
-      throw new AuthenticationError("Your session expired. Sign in again.");
+    /**
+     * Authentitication
+     */
+    const token = req.headers && req.headers.authorization ? req.headers['authorization'] : null;
+    let user = null;
+    if (token) {
+        try {
+            user = await jwt.verify(token, process.env.SECRET_KEY);
+        } catch (e) {
+            throw new AuthenticationError('Your session expired. Sign in again.');
+        }
     }
-  }
-  // else {
-  //   throw new AuthenticationError("Token is required");
-  // }
+    // else {
+    //   throw new AuthenticationError("Token is required");
+    // }
 
-  /**
-   * Loaders
-   */
-  var loaders = {
-    user: new DataLoader(keys => dataLoaders.user.batchUsers(keys, models)),
-    group: new DataLoader(keys => dataLoaders.group.barchGroups(keys, models)),
-    projectStatus: new DataLoader(keys =>
-      dataLoaders.projectStatus.batchStatus(keys, models)
-    )
-  };
+    /**
+     * Loaders
+     */
+    var loaders = {
+        user: new DataLoader(keys => dataLoaders.user.batchUsers(keys, models)),
+        group: new DataLoader(keys => dataLoaders.group.barchGroups(keys, models)),
+        projectStatus: new DataLoader(keys => dataLoaders.projectStatus.batchStatus(keys, models))
+    };
 
-  return {
-    me: user,
-    loaders,
-    secret: process.env.SECRET_KEY
-  };
+    return {
+        me: user,
+        loaders,
+        secret: process.env.SECRET_KEY
+    };
 };
 
 // Set up Apollo Server
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      UserAPI: new UserAPI({ store: models }),
-      ProjectAPI: new ProjectAPI({ store: models }),
-      MemberAPI: new MemberAPI({ store: models }),
-      ProjectStatusAPI: new ProjectStatusAPI({ store: models }),
-      GroupAPI: new GroupAPI({ store: models }),
-      FeatureAPI: new FeatureAPI({ store: models }),
-      IssueAPI: new IssueAPI({ store: models })
-    };
-  },
-  context,
-  formatError: error => {
-    // console.log(err);
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            UserAPI: new UserAPI({ store: models }),
+            ProjectAPI: new ProjectAPI({ store: models }),
+            MemberAPI: new MemberAPI({ store: models }),
+            ProjectStatusAPI: new ProjectStatusAPI({ store: models }),
+            GroupAPI: new GroupAPI({ store: models }),
+            FeatureAPI: new FeatureAPI({ store: models }),
+            IssueAPI: new IssueAPI({ store: models })
+        };
+    },
+    context,
+    formatError: error => {
+        // console.log(err);
 
-    if (error.message.startsWith("Database Error: ")) {
-      return new Error("Internal server error");
-    }
+        if (error.message.startsWith('Database Error: ')) {
+            return new Error('Internal server error');
+        }
 
-    // remove the internal sequelize error message
-    // leave only the important validation error
-    const message = error.message
-      .replace("SequelizeValidationError: ", "")
-      .replace("Validation error: ", "");
+        // remove the internal sequelize error message
+        // leave only the important validation error
+        const message = error.message
+            .replace('SequelizeValidationError: ', '')
+            .replace('Validation error: ', '');
 
-    return {
-      ...error,
-      message
-    };
-  },
-  // formatResponse: res => {
-  //   console.log(res);
-  // },
-  debug: is_production
+        return {
+            ...error,
+            message
+        };
+    },
+    // formatResponse: res => {
+    //   console.log(res);
+    // },
+    debug: is_production
 });
 
 // Start our server if we're not in a test env.
 // if we're in a test env, we'll manually start it in a test
 
 sequelize
-  .sync({ force: false })
-  .then(async () => {
-    if (process.env.NODE_ENV !== "test")
-      server
-        .listen({ port: 5000 })
-        .then(({ url }) => console.log(`🚀 app running at ${url}`));
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .sync({ force: false })
+    .then(async () => {
+        if (process.env.NODE_ENV !== 'test')
+            server
+                .listen({ port: 5000 })
+                .then(({ url }) => console.log(`🚀 app running at ${url}`));
+    })
+    .catch(err => {
+        console.log(err);
+    });
