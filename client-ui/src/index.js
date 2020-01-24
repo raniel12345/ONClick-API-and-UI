034a69f432ca414b08e66ed7c9528b66bdc0bb30
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-01-24 22:17:39 +0800

    Setup initial configuration for Apollo Client

--- client-ui/src/index.js
+++ client-ui/src/index.js
@@ -3,13 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import { resolvers, typeDefs } from './resolvers';
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+    cache,
+    link: new HttpLink({
+        uri: 'http://localhost:5000/',
+        headers: {
+            authorization: localStorage.getItem('token') ? localStorage.getItem('token') : '',
+            'client-name': 'ONClick [web]',
+            'client-version': '1.0.0'
+        }
+    }),
+    resolvers,
+    typeDefs
+});
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token')
+        // projects: []
+    }
+});
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <ApolloProvider client={client}>
+        <Router>
+            <App />
+        </Router>
+    </ApolloProvider>,
     document.getElementById('root')
 );
 
