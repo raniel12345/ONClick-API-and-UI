04a1bf6b5a57599e5ea59b5ce2a164278c3ed501
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-01-26 21:59:22 +0800

    Create new separate custom component - to modify the url on login and main container

--- client-ui/src/App.js
+++ client-ui/src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import gql from 'graphql-tag';
@@ -6,6 +8,32 @@ import { useQuery } from '@apollo/react-hooks';
 
 import AppContainer from './components/AppContainer';
 import SignIn from './components/Container/SignIn';
+
+function SignInRouter() {
+    return (
+        <Fragment>
+            <Switch>
+                <Route path="/sign-in">
+                    <SignIn />
+                </Route>
+            </Switch>
+            <Redirect to="/sign-in" />
+        </Fragment>
+    );
+}
+
+function MainRouter() {
+    return (
+        <Fragment>
+            <Switch>
+                <Route path="/">
+                    <AppContainer />
+                </Route>
+            </Switch>
+            <Redirect to="/" />
+        </Fragment>
+    );
+}
 
 // localStorage.clear();
 
@@ -19,10 +47,10 @@ function IsLoggedIn() {
     const { data } = useQuery(IS_LOGGED_IN);
     const { isLoggedIn } = data;
 
-    return isLoggedIn ? <AppContainer /> : <SignIn />;
+    return isLoggedIn ? <MainRouter /> : <SignInRouter />;
 }
 
-function App() {
+function App(props) {
     return (
         <Fragment>
             <CssBaseline />
